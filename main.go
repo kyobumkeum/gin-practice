7ee1145134ac81390506dab4657bfcb517232113
kyobum.keum <kyobum.keum@gmail.com>
2021-10-20 17:25:07 +0900

    init

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := createServer()
+	server.ListenAndServe()
+}
+
+func createServer() *http.Server {
+	return &http.Server{
+		Addr:           "localhost:80",
+		Handler:        createRouter(),
+		ReadTimeout:    0,
+		WriteTimeout:   0,
+		MaxHeaderBytes: 1 << 20,
+	}
+}
+
+func createRouter() *gin.Engine {
+	r := gin.New()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	return r
+}
